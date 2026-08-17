d0153d256953d446d78d92a1250903face0e8367
Joel <jcjoelchiu@gmail.com>
2026-08-17 12:56:38 +0200

    [user profile] display simple user information

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,21 @@
+'use client'
+
+import { useAuth } from "@/context/AuthContext"
+
 export default function Page() {
+    const {user, loading} = useAuth();
+
+
+    if (loading) return <div>Loading...</div>;
+
+    if (!user) return <p>not logged in</p>;
+
     return (
-        <h1>User Profile</h1>
+        <div>
+            <h1>User Profile</h1>
+            <p>UserID: {user.uid}</p>
+            <p>UserName: {user.email}</p>
+        </div>
+        
     )
 }
